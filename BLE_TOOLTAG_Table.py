6cded3755ad77340e03d59318103fad0144ab4b6
ali <ali@geometris.com>
2023-11-08 17:48:56 -0600

    More featuers

--- BLE_TOOLTAG_Table.py
+++ BLE_TOOLTAG_Table.py
@@ -1,18 +1,32 @@
 import PySimpleGUI as sg
 
+# Initialize data list to store input data
+data = []
+
+# Define the layout
 layout = [
-    [sg.Text('Select an option:')],
-    [sg.Combo(['Option 1', 'Option 2'], default_value='Option 1', key='combo')],
-    [sg.Text(size=(40, 1), key='-OUTPUT-')]
+    [sg.Text('Enter input:'), sg.InputText(key='-INPUT-'), sg.Button('Add')],
+    [sg.Table(values=data, headings=['Input Data'], key='-TABLE-', auto_size_columns=False,
+              display_row_numbers=True, col_widths=[30])]
 ]
 
-window = sg.Window('Combo Example', layout)
+# Create the window
+window = sg.Window('Input Table', layout)
 
+# Event loop
 while True:
     event, values = window.read()
-    if event in (sg.WIN_CLOSED, 'Cancel'):
-        break
-    selected_option = values['combo']
-    window['-OUTPUT-'].update(f'You selected {selected_option}')
 
+    # Close the window if the 'X' button is clicked
+    if event == sg.WIN_CLOSED:
+        break
+
+    # Append the input data to the table
+    if event == 'Add':
+        input_data = values['-INPUT-']
+        if input_data:
+            data.append([input_data])
+            window['-TABLE-'].update(values=data)
+
+# Close the window
 window.close()
